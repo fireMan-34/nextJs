a36192c0a1644a9ab1b734ef6ba226b0dc61da90
maxufeng <unf-apqb330d4@dingtalk.com>
2024-04-19 13:43:18 +0800

    feat: 添加环境变量

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <h2>Next</h2>
+      <p>Route mode: {process.env.ROUTE_MODE}</p>
       <body className={inter.className}>
         {children}
       </body>
