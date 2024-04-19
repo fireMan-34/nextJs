7037562b5280abe89b6f15e6951ab3b402a14a3b
maxufeng <unf-apqb330d4@dingtalk.com>
2024-04-19 13:34:27 +0800

    feat: App 路由

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+
+function AboutPage() {
+  return <div>
+    <h2>Hello, About Page</h2>
+  </div>
+};
+
+export default AboutPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <h2>Next</h2>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
