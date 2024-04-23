321d1bd3e5c5d11335172fd05551eb4768264309
maxufeng <unf-apqb330d4@dingtalk.com>
2024-04-23 18:46:31 +0800

    feat: 添加工程优化

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // https://nextjs.org/docs/app/building-your-application/configuring/typescript#typescript-plugin
+    typedRoutes: true,
+  }
+};
 
 export default nextConfig;
