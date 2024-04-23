20fe7406a6fa51bab68d106601d28b9964f68c9e
maxufeng <unf-apqb330d4@dingtalk.com>
2024-04-23 11:36:28 +0800

    feat: 全局视图文件

---
+++ app/global-error.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function GlobalErrorPage() {
+  return <main>
+    Global Error
+  </main>
+}
---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function LoadingPage() {
+  return <main className="m-auto" >Loading ...</main>
+}
